3e523aacf762baae4330015df0051d6e5d7eac39
GajananHegde <gajanan.hegde@galepartners.com>
2022-11-20 19:53:05 +0530

    Changes

--- project-Jenkins/project.groovy
+++ project-Jenkins/project.groovy
@@ -46,13 +46,13 @@ def mainfunc(String parallel_stage, String param12, String param13){
         case 'Frontend':
             echo "We are in the frontend section"
             // def stringArray=["one","two","three"]
-            String command = ""
-            for ( str in stringArray )
-            {
-                command = command + str + " "
-            }
+            // String command = ""
+            // for ( str in stringArray )
+            // {
+            //     command = command + str + " "
+            // }
             // env.stringSize=stringArray.size()
-            echo command
+            // echo command
             echo param12
             echo param13
             inject_env("dev")
