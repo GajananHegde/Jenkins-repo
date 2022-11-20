fe45527dcc9fd89c64507cc3298d30f7028fad40
GajananHegde <gajanan.hegde@galepartners.com>
2022-11-20 21:56:55 +0530

    Changes

--- project-Jenkins/project.groovy
+++ project-Jenkins/project.groovy
@@ -15,7 +15,7 @@ def inject_env (String build_branch){
     env.deploy_test_var1='Var 1 - Hello'
     env.deploy_test_var2='Var 2 - World'
     env.environ_file='.Build-Dir/Test-2/.build/env'
-    env.env_file_name='p-bmo-commercial-nginx-prod-fr-app-1 p-bmo-commercial-nginx-prod-fr-app-2'
+    env.env_file_name="'p-bmo-commercial-nginx-prod-fr-app-1' 'p-bmo-commercial-nginx-prod-fr-app-2'"
     env.aws_region='us-west-2'
     env.stringArray="one,two,three"
     for_loop_test(env.stringArray)
