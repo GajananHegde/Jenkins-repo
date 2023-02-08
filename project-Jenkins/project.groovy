5a8f6e6089ede03ddd08b68ca25c4bd8ecf6b19b
GajananHegde <gajanan.hegde@galepartners.com>
2023-02-08 16:29:47 +0530

    Changes

--- project-Jenkins/project.groovy
+++ project-Jenkins/project.groovy
@@ -57,7 +57,7 @@ def inject_stage (String build_branch){
 
 
 // def mainfunc(String build_branch, String build_number, String build_job, String build_url) {
-def mainfunc(String[] choices, String param){
+def mainfunc(String choices, String param){
     echo "-- ENV for building - ${param}"
     switch(parallel_stage){
         case 'Frontend':
