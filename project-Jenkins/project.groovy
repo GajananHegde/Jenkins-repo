90b72660cbd807db7e08a5a8e84d05dafcd31950
GajananHegde <gajanan.hegde@galepartners.com>
2023-05-25 18:55:14 +0530

    nginx reload test

--- project-Jenkins/project.groovy
+++ project-Jenkins/project.groovy
@@ -17,7 +17,7 @@ def inject_env (String variable1){
     env.deploy_test_var2='Var 2 - World'
     env.environ_file='.Build-Dir/project-Jenkins/.build/env'
     env.nginx_file_path = '.Build-Dir/project-Jenkins/.build/'
-    env.docker_compose_file_nginx='docker-compose-nginx.yml'
+    env.docker_compose_file_nginx='.Build-Dir/project-Jenkins/.build/docker-compose-nginx.yml'
     env.env_file_name="\'p-bmo-commercial-nginx-us-redirect-prod-app-1\' \'p-bmo-commercial-nginx-us-redirect-prod-app-2\'"
     env.aws_region='us-west-2'
     // test_cli_command(env.env_file_name)
@@ -26,16 +26,12 @@ def inject_env (String variable1){
     // print(env.stringArray instanceof String)
     sh """
     echo "${variable1} this is the test"
-    cd ${nginx_file_path}
-    pwd
     export PATH=/usr/local/bin
     /usr/local/bin/docker-compose -f ${docker_compose_file_nginx} down
     /bin/sleep 5
     /usr/local/bin/docker-compose -f ${docker_compose_file_nginx} up -d
     /bin/sleep 5
-    /bin/rm -rf conf.d
-    /bin/mkdir conf.d
-    /bin/cp -r actual_conf/* conf.d/
+    /bin/rm -rf conf.d && /bin/mkdir conf.d && /bin/cp -r actual_conf/* conf.d/
     /usr/local/bin/docker-compose -f ${docker_compose_file_nginx} exec -T nginx nginx -s reload
     /bin/sleep 5
     /usr/local/bin/docker-compose -f ${docker_compose_file_nginx} down
