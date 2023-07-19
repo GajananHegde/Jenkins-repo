d6464d250c7ffcd1ea8a6cc3a7047a5cdd9dcd6a
GajananHegde <gajanan.hegde@galepartners.com>
2023-07-19 13:15:27 +0530

    scp command testing

--- project-Jenkins/project.groovy
+++ project-Jenkins/project.groovy
@@ -85,7 +85,7 @@ def inject_stage (String build_branch){
 def scp_load_test()
 {
     sh """
-        rsync -r -o "StrictHostKeyChecking=no" ${maintenance_page_dir} ${ssh_username}@${deploy_ssh_host}:${nginx_conf_dir}
+        rsync -e \"StrictHostKeyChecking=no\" -avh ${maintenance_page_dir}/ ${ssh_username}@${deploy_ssh_host}:${nginx_conf_dir}/
     """
     // ssh -o \"StrictHostKeyChecking=no\" ${ssh_username}@${deploy_ssh_host} \"cd .Build-Dir/project-Jenkins/.build/ && rm -rf maintenance
 }
